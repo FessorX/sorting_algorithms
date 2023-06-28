6fdd017b0de53b3c4cb55a1d6deba47d673b9342
Musa Abraham <profaimusa@gmail.com>
2023-06-28 03:16:55 -0700

    more

--- sort.h
+++ sort.h
@@ -30,7 +30,7 @@ int *rand_array(int len, int max);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size
+void quick_sort(int *array, size_t size;
 
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
@@ -43,4 +43,4 @@ void heap_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
-#endif
+#endif /*_SORT_H*/
